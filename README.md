a5cbb45cb893164bcc69010dab2faec1df545c80
Carlos Alberto Mata Gil <CeuxDruman@gmail.com>
2016-12-14 17:04:39 +0100

    Project 01 with Gradle
    Added proyect 01 using Gradle, jgrapht dependency and pmd and clover plugins. The project is the same used in https://github.com/CeuxDruman/EGC-Maven-Classwork-Testing

---
+++ README.md
@@ -0,0 +1,4 @@
+__WARNING__: You can test Clover plugin is working by using the command 'gradle cloverGenerateReport', but it'll throw an error because you need a valid License:
+```
+> java.lang.RuntimeException: Invalid or missing License.. Please visit http://my.atlassian.com to obtain a valid license.
+```
---
+++ src/main/java/org/egc/App.java
@@ -0,0 +1,24 @@
+package org.egc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+    }
+    
+    public class JumbledIncrementerRule1 {
+        public void foo() {
+            for (int i = 0; i < 10; i++) {
+                for (int k = 0; k < 20; i++) {
+                    System.out.println("Hello");
+                }
+            }
+        }
+    }
+}
